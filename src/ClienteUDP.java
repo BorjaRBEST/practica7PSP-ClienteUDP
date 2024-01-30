73cf5277065510e0002ce8c6471346e541a8bef6
Borja Rodriguez <borjarodriguez203@gmail.com>
2024-01-30 07:25:51 +0100

    Realizamos últimos cambios y guardamos

--- src/ClienteUDP.java
+++ src/ClienteUDP.java
@@ -1,5 +1,31 @@
+import java.net.*;
+import java.io.*;
+
 public class ClienteUDP {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final String servidorIP = "localhost"; // Cambia esto si el servidor se encuentra en otro equipo
+        final int puerto = 12345; // Puerto en el que está escuchando el servidor
+
+        try (DatagramSocket socket = new DatagramSocket()) {
+            InetAddress servidorDireccion = InetAddress.getByName(servidorIP);
+
+            int numero = 5; // Cambia el número a tu elección
+            String mensaje = Integer.toString(numero);
+            byte[] sendData = mensaje.getBytes();
+
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, servidorDireccion, puerto);
+
+            socket.send(sendPacket); // Envía el número al servidor
+
+            byte[] receiveData = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+            socket.receive(receivePacket); // Espera a recibir la respuesta del servidor
+
+            String respuesta = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            System.out.println("Respuesta del servidor: " + respuesta);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
